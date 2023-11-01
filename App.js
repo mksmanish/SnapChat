686a180db4f412aa9ca4c2df7be03919a9dd9dfa
manish <manish123kumar79@gmail.com>
2023-11-01 12:06:55 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Apps</Text>
+        <Text>manish</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
