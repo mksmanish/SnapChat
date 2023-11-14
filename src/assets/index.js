b955344954c7c08955af7fec4f2c686bbfede94c
manish <manish123kumar79@gmail.com>
2023-11-14 08:00:53 +0530

    icons added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,4 @@
 export const snapImage = require("./images/imageSnap.jpg")
+export const arrow_back = require("./icons/arrow_back.png")
+export const eye = require("./icons/eye.png")
+export const eyeoff = require("./icons/eyeoff.png")
