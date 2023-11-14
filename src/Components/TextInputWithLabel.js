8e5aac373430207e9f564971d9d318ee948c50f2
manish <manish123kumar79@gmail.com>
2023-11-14 21:26:50 +0530

    hide and show button

--- src/Components/TextInputWithLabel.js
+++ src/Components/TextInputWithLabel.js
@@ -1,6 +1,7 @@
-import {StyleSheet, Text, View, TextInput} from 'react-native';
+import {StyleSheet, Text, View, TextInput, TouchableOpacity, Image} from 'react-native';
 import React from 'react';
 import colors from '../assets/styles/colors';
+import { eye, eyeoff } from '../assets';
 
 const TextInputWithLabel = ({
   label,
@@ -9,18 +10,27 @@ const TextInputWithLabel = ({
   onChangeText,
   inputStyle,
   textStyle,
+  secureText,
+  onPressSecure,
   ...props
 }) => {
   return (
-    <View style={styles.container}>
+    <View >
       <Text style={styles.labelStyle}>{label}</Text>
+      <View style={{...styles.inputStyle, ...inputStyle}}>
       <TextInput
         value={value}
         placeholder={placeholder}
         onChangeText={onChangeText}
-        style={{...styles.inputStyle, ...inputStyle}}
+        style={{...styles.labelStyle}}
         {...props}
       />
+      {onPressSecure && (
+        <TouchableOpacity onPress={onPressSecure}>
+        <Image source={ secureText ? eyeoff : eye} style={{height:20,width:20,marginTop:10}}/>
+      </TouchableOpacity>
+      )}
+      </View>
     </View>
   );
 };
@@ -37,10 +47,13 @@ const styles = StyleSheet.create({
   inputStyle: {
     borderBottomWidth: 2,
     padding:5,
-    borderBottomColor:colors.blue
+    borderBottomColor:colors.blue,
+    flexDirection:"row",
+    justifyContent:"space-between"
   },
   labelStyle:{
     fontSize:15,
     fontWeight:"500",
+    flex:1
   }
 });
--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -1,18 +1,21 @@
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 import React, {useState} from 'react';
 import Loader from '../../Components/Loader';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Header from '../../Components/Header';
 import TextInputWithLabel from '../../Components/TextInputWithLabel';
 import Strings from '../../constants/language/Strings';
+import { moderateScale } from 'react-native-size-matters';
+import colors from '../../assets/styles/colors';
 
 const Login = () => {
   const [state, setState] = useState({
     isLoading: false,
     email: '',
     password: '',
+    isSecure:true,
   });
-  const {isLoading, email, password} = state;
+  const {isLoading, email, password,isSecure} = state;
   const updateState = (data) => setState((state) => ({...state,...data}))
   return (
     <View style={{flex: 1, padding: 24}}>
@@ -24,6 +27,7 @@ const Login = () => {
           placeholder={Strings.EMAIL}
           value={email}
           onChangeText={(email) => updateState({email})}
+          secureText={false}
 
         />
         <View style={{margin: 10}} />
@@ -32,9 +36,13 @@ const Login = () => {
           placeholder={Strings.PASSWORD}
           value={password}
           onChangeText={(password) => updateState({password})}
-          secureTextEntry={true}
-          
+          secureTextEntry={isSecure}
+          secureText={isSecure}
+          onPressSecure={() => updateState({isSecure : !isSecure})}
         />
+        <TouchableOpacity style={{justifyContent:"center",alignItems:"center",marginVertical:moderateScale(20)}}>
+          <Text style={{fontSize:16,fontWeight:"500",color:colors.blue}}>Forgot your password ?</Text>
+          </TouchableOpacity>
       </SafeAreaView>
     </View>
   );
